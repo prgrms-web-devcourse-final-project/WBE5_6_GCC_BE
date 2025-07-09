bc629d1d7176a7b7ba93d5129ebce55637d24633
IDIOcoder <newon.jsw@gmail.com>
2025-07-10 00:29:22 +0900

    :recycle: refactor: example message of api response

--- src/main/java/com/honlife/core/infra/response/CommonApiResponse.java
+++ src/main/java/com/honlife/core/infra/response/CommonApiResponse.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record CommonApiResponse<T>(
     @Schema(description = "서비스 정의 코드", example = "2000")
     String code,
-    @Schema(description = "응답 메시지", example = "성공적으로 처리되었습니다.")
+    @Schema(description = "응답 메시지", example = "OK")
     String message,
     @Schema(description = "응답 데이터")
     T data
