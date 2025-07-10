3761abc39f8e2d13a28d179d9eddfd91b08f12b1
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-10 11:19:00 +0900

    refactor: update ApiEntityResponse structure

--- src/main/java/com/honlife/core/infra/response/ApiEntityResponse.java
+++ src/main/java/com/honlife/core/infra/response/ApiEntityResponse.java
@@ -3,12 +3,18 @@ package com.honlife.core.infra.response;
 public class ApiEntityResponse {
   private int status;
   private String message;
+   private Object data;
 
   public ApiEntityResponse(int status, String message) {
     this.status = status;
     this.message = message;
   }
 
+  public ApiEntityResponse(int status, String message, Object data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
   public int getStatus() {
     return status;
   }
