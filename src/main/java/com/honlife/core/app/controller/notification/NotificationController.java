8997857f032eeacc0f8bcaed9f0dd6804b8af389
Jun <wnsur1234@gmail.com>
2025-07-10 22:04:23 +0900

    :sparkles: feat: add API for toggling notification settings

--- src/main/java/com/honlife/core/app/controller/notification/NotificationController.java
+++ src/main/java/com/honlife/core/app/controller/notification/NotificationController.java
@@ -1,11 +1,18 @@
 package com.honlife.core.app.controller.notification;
 
+import com.honlife.core.app.controller.notification.payload.NotificationPayload;
+import com.honlife.core.infra.response.CommonApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.honlife.core.app.model.notification.dto.NotificationDTO;
 import com.honlife.core.app.model.notification.service.NotificationService;
 
-
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "설정",description = "설정관련 API 입니다.")
 @RestController
 @RequestMapping(value = "/api/notifications", produces = MediaType.APPLICATION_JSON_VALUE)
 public class NotificationController {
@@ -26,6 +34,16 @@ public class NotificationController {
 
     public NotificationController(final NotificationService notificationService) {
         this.notificationService = notificationService;
+    }
+
+    @Operation(summary = "알림 설정 변경", description = "알림 종류(key)와 ON/OFF 여부(isActive)를 전달받아 설정을 변경합니다.")
+    @PutMapping
+    public ResponseEntity<CommonApiResponse<Void>> onOffNotification(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody NotificationPayload payload
+            ){
+        return ResponseEntity.ok(CommonApiResponse.noContent());
+
     }
 
     @GetMapping
