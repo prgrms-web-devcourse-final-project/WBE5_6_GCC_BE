bb09d1da6786d2df557f00c906dae489c1a8630c
Jun <wnsur1234@gmail.com>
2025-07-10 16:36:37 +0900

    :sparkles: feat: create notificationPayload

--- src/main/java/com/honlife/core/app/controller/notification/payload/NotificationPayload.java
+++ src/main/java/com/honlife/core/app/controller/notification/payload/NotificationPayload.java
@@ -1,4 +1,13 @@
 package com.honlife.core.app.controller.notification.payload;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class NotificationPayload {
+    private String type;
+    private Boolean onOff;
 }
