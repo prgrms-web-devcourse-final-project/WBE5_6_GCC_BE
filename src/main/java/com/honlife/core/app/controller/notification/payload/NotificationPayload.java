814ce473880d0d5f9949e3c5ce3586cc8f2df594
Jun <wnsur1234@gmail.com>
2025-07-10 22:04:23 +0900

    :sparkles: feat: add API for toggling notification settings

--- src/main/java/com/honlife/core/app/controller/notification/payload/NotificationPayload.java
+++ src/main/java/com/honlife/core/app/controller/notification/payload/NotificationPayload.java
@@ -1,5 +1,6 @@
 package com.honlife.core.app.controller.notification.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,16 @@ import lombok.Setter;
 @Setter
 @Builder
 public class NotificationPayload {
+
+    @Schema(
+            description = "조작을 원하는 알람 타입입니다.",
+            example = "EMAIL",
+            allowableValues = { "EMAIL", "ROUTINE", "CHALLENGE" }
+    )
     private String type;
+    @Schema(
+            description = "알림 ON/OFF 여부입니다.",
+            example = "true"
+    )
     private Boolean onOff;
 }
