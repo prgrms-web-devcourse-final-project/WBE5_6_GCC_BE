55244c28a6c4800da3506df070111631dacda1d2
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-10 10:14:17 +0900

    feat(Item): add ItemCreateRequestDTO for creating item

---
+++ src/main/java/com/honlife/core/app/model/item/dto/ItemCreateRequestDTO.java
@@ -0,0 +1,32 @@
+package com.honlife.core.app.model.item.dto;
+
+import com.honlife.core.app.model.item.code.ItemType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(name = "ItemCreateRequestDTO", description = "상점 아이템 추가 요청 DTO")
+public class ItemCreateRequestDTO {
+
+  @NotNull
+  @Size(max = 50)
+  @Schema(description = "아이템 키 (고유값)", example = "item_clean_001")
+  private String itemKey;
+
+  @NotNull
+  @Size(max = 50)
+  @Schema(description = "아이템 이름", example = "깔끔 청소 도우미")
+  private String name;
+
+  @NotNull
+  @Schema(description = "아이템 가격", example = "300")
+  private Integer price;
+
+  @NotNull
+  @Schema(description = "아이템 타입", example = "CONSUMABLE")
+  private ItemType type;
+}
