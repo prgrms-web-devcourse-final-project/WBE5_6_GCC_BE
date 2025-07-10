ac516cf034daa3acd96672d3de66a65a2ca9cb77
Jun <wnsur1234@gmail.com>
2025-07-10 16:35:35 +0900

    :fire: reset: NotificationController to match mock branch

--- src/main/java/com/honlife/core/app/controller/notification/payload/NotificationPayload.java
+++ src/main/java/com/honlife/core/app/controller/notification/payload/NotificationPayload.java
@@ -1,17 +1,4 @@
 package com.honlife.core.app.controller.notification.payload;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Builder
 public class NotificationPayload {
-
-    private Long notificationId;
-
-    private Long memberId;
-
-
 }
