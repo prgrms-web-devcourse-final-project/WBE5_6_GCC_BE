faabc9bae7636b9ca127893c3d72e96baa367f84
IDIOcoder <newon.jsw@gmail.com>
2025-07-12 22:21:53 +0900

    :sparkles: add: update password request dto

--- src/main/java/com/honlife/core/app/controller/member/payload/MemberUpdatePasswordRequest.java
+++ src/main/java/com/honlife/core/app/controller/member/payload/MemberUpdatePasswordRequest.java
@@ -1,5 +1,13 @@
 package com.honlife.core.app.controller.member.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class MemberUpdatePasswordRequest {
 
+    @NotBlank
+    private String oldPassword;
+    @NotBlank
+    private String newPassword;
 }
