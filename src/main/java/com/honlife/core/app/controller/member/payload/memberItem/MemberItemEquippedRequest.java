905e409b7d417d08f3284194bfc58fd7300358e2
Kminseo <childstone@users.noreply.github.com>
2025-07-12 23:40:58 +0900

    ✨ feat: extend item equip API to support full use cases
    아이템 착용상태 변경 시나리오 3가지를 고려하여 작성하였습니다.

---
+++ src/main/java/com/honlife/core/app/controller/member/payload/memberItem/MemberItemEquippedRequest.java
@@ -0,0 +1,17 @@
+package com.honlife.core.app.controller.member.payload.memberItem;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MemberItemEquippedRequest {
+    @Schema(description = "착용하고 있던 아이템", example = "top_item_01")
+    private String oldItemKey;
+
+    @Schema(description = "새로 착용 할 아이템", example = "top_item_02")
+    private String newItemKey;
+}
