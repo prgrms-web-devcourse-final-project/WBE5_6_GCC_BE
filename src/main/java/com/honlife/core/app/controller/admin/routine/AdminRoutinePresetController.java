4bff63f06d14bc4bbfe322f9ff3669f9966d1a9e
DY-Tempus <2744ch@naver.com>
2025-07-14 00:31:34 +0900

    refactor: change preset creation to return noContent

--- src/main/java/com/honlife/core/app/controller/admin/routine/AdminRoutinePresetController.java
+++ src/main/java/com/honlife/core/app/controller/admin/routine/AdminRoutinePresetController.java
@@ -18,15 +18,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -179,7 +176,7 @@ public class AdminRoutinePresetController {
           "*실제 DB에 반영되지 않음*"
   )
   @PostMapping
-  public ResponseEntity<CommonApiResponse<Long>> createRoutinePreset(
+  public ResponseEntity<CommonApiResponse<Void>> createRoutinePreset(
       @RequestBody @Valid final AdminRoutinePresetSaveRequest request,
       BindingResult bindingResult
   ) {
@@ -195,9 +192,8 @@ public class AdminRoutinePresetController {
     // 3. routinePresetService.create() 호출
     // 4. 생성된 ID 반환
 
-    Long createdId = 999L; // 모킹 ID
     return ResponseEntity.status(HttpStatus.CREATED)
-        .body(CommonApiResponse.success(createdId));
+        .body(CommonApiResponse.noContent());
   }
 
   /**
