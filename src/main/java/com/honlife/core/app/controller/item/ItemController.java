804fb910421af7060bf6920eca57f4347367c959
Jun <wnsur1234@gmail.com>
2025-07-15 10:39:05 +0900

    :recycle: refactor: Remove Swagger annotations and add Lombok

--- src/main/java/com/honlife/core/app/controller/item/ItemController.java
+++ src/main/java/com/honlife/core/app/controller/item/ItemController.java
@@ -2,21 +2,13 @@ package com.honlife.core.app.controller.item;
 
 import com.honlife.core.app.controller.item.payload.ItemResponse;
 import com.honlife.core.app.model.item.code.ItemType;
-import com.honlife.core.app.model.item.domain.Item;
-import com.honlife.core.app.model.item.dto.ItemDTO;
 import com.honlife.core.app.model.item.service.ItemService;
 import com.honlife.core.infra.response.CommonApiResponse;
 import com.honlife.core.infra.response.ResponseCode;
-import com.honlife.core.infra.util.ReferencedException;
-import com.honlife.core.infra.util.ReferencedWarning;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,8 +20,6 @@ import java.util.List;
 
 
 
-@SecurityRequirement(name = "bearerAuth")
-@Tag(name = "아이템", description = "아이템 관련 api 입니다.")
 @RestController
 @RequestMapping(value = "/api/v1/items", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ItemController {
@@ -47,7 +37,6 @@ public class ItemController {
     @GetMapping
     @Operation(summary = "아이템 조회", description = "전체 아이템 조회 또는 type 값을 통해 특정 아이템만 조회할 수 있습니다.")
     public ResponseEntity<CommonApiResponse<List<ItemResponse>>> getAllItems(
-            @Parameter(description = "조회할 아이템 타입", example = "TOP")
             @RequestParam(value = "type", required = false) ItemType itemType
     ) {
 
@@ -94,10 +83,8 @@ public class ItemController {
      * @param itemKey 아이템 고유 아이다
      * @return ItemResponse itemKey 값과 일치하는 아이템 정보 반환
      */
-    @Operation(summary = "아이템 단건 조회", description = "아이템 key 값을 통해 특정 아이템을 조회합니다.")
     @GetMapping("/{key}")
     public ResponseEntity<CommonApiResponse<ItemResponse>> getItemByKey(
-            @Parameter(description = "아이템 Key 값", example = "top_item_01")
             @PathVariable("key") String itemKey) {
 
         ItemResponse item = ItemResponse.builder()
@@ -117,10 +104,8 @@ public class ItemController {
      * 아이템 구매 API
      * @param itemId 아이템 고유 아이다
      */
-    @Operation(summary = "아이템 구매", description = "포인트를 차감하고 아이템을 구매합니다.")
     @PostMapping("/{key}")
     public ResponseEntity<CommonApiResponse<Void>> getItem(
-            @Parameter(name = "key", description = "구매할 아이템의 key", example = "top_item_01")
             @PathVariable("key") String itemKey,
             @AuthenticationPrincipal UserDetails userDetails
     ) {
--- src/main/java/com/honlife/core/app/controller/item/payload/ItemResponse.java
+++ src/main/java/com/honlife/core/app/controller/item/payload/ItemResponse.java
@@ -1,22 +1,19 @@
 package com.honlife.core.app.controller.item.payload;
 
-
 import com.honlife.core.app.model.item.code.ItemType;
-import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ItemResponse {
     private Long itemId;
 
-    @Schema(description = "아이템 타입", example = "TOP",
-            allowableValues = {"TOP", "BOTTOM", "ACCESSORY"})
     private ItemType itemType;
-
 
     private String itemKey;
 
