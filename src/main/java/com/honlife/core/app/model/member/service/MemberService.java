c01dde58b91279c6882b063087570d451992f765
Kminseo <childstone@users.noreply.github.com>
2025-07-15 15:16:29 +0900

    🐛 fix: re-add findMemberByEmail missed in merge
    merge 중 memberService에서 사라진 메소드를 복구하였습니다.

--- src/main/java/com/honlife/core/app/model/member/service/MemberService.java
+++ src/main/java/com/honlife/core/app/model/member/service/MemberService.java
@@ -246,4 +246,15 @@ public class MemberService {
         // 관심카테고리 정보 업데이트
         interestCategoryService.updateInterestCategory(member, signupRequest.getInterestedCategoryIds());
     }
+
+
+    /**
+     * 회원의 이메일을 받아 회원 정보를 리턴하는 메소드
+     * @param userEmail 현재 로그인한 회원의 이메일
+     * @return 회원의 정보가 없다면 null을, 있다면 회원의 정보를 담은 dto를 반환합니다.
+     */
+    public MemberDTO findMemberByEmail(String userEmail){
+        Member targetMember = memberRepository.findByEmailAndIsActive(userEmail, true).orElse(null);
+        return mapper.map(targetMember, MemberDTO.class);
+    }
 }
