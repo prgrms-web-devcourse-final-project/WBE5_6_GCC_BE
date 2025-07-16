5b3a028caed594c61b17878d70dc791adca5e261
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-17 00:33:18 +0900

    feat: Add Swagger response schema for weekly and daily routine APIs

---
+++ src/main/java/com/honlife/core/app/controller/routine/payload/RoutineItemDTO.java
@@ -0,0 +1,43 @@
+package com.honlife.core.app.controller.routine.payload;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@Schema(description = "루틴 아이템")
+public class RoutineItemDTO {
+
+
+  @Schema(description = "스케줄 ID", example = "3")
+  private Long scheduleId;
+
+  @Schema(description = "루틴 ID", example = "3")
+  private Long routineId;
+
+  @Schema(description = "상위 카테고리", example = "건강")
+  private String majorCategory;
+
+  @Schema(description = "하위 카테고리", example = "운동")
+  private String subCategory;
+
+  @Schema(description = "루틴 이름", example = "저녁 조깅")
+  private String name;
+
+  @Schema(description = "루틴 실행 시간", example = "19:00")
+  private String triggerTime;
+
+  @Schema(description = "완료 여부", example = "false")
+  private Boolean isDone;
+
+  @Schema(description = "중요 여부", example = "true")
+  private Boolean isImportant;
+
+  @Schema(description = "해당 날짜", example = "true")
+  private Boolean date;
+
+}
