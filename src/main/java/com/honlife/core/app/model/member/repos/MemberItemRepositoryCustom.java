4aef34099130fe7a8cc8542db883dff04c2cf368
Jun <wnsur1234@gmail.com>
2025-07-18 08:59:10 +0900

    :construction: wip: Temporarily save current progress for branch switching

---
+++ src/main/java/com/honlife/core/app/model/member/repos/MemberItemRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.honlife.core.app.model.member.repos;
+
+import com.honlife.core.app.model.item.code.ItemType;
+import com.querydsl.core.Tuple;
+
+import java.util.List;
+
+public interface MemberItemDslRepository {
+    List<Tuple> findMemberItems(Long memberId, ItemType itemType);
+    List<Tuple> findEquippedMemberItems(Long memberId);
+}
---
+++ src/main/java/com/honlife/core/app/model/member/repos/MemberItemRepositoryCustomImpl.java
@@ -0,0 +1,47 @@
+package com.honlife.core.app.model.member.repos;
+
+import com.honlife.core.app.model.item.code.ItemType;
+import com.honlife.core.app.model.item.domain.QItem;
+import com.honlife.core.app.model.member.domain.QMemberItem;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class MemberItemDslRepositoryImpl implements MemberItemDslRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    QMemberItem memberItem = QMemberItem.memberItem;
+    QItem item = QItem.item;
+
+    @Override
+    public List<Tuple> findMemberItems(Long memberId, ItemType itemType) {
+        return queryFactory
+            .select(memberItem, item)
+            .from(memberItem)
+            .join(item).on(memberItem.item.id.eq(item.id))
+            .where(
+                memberItem.member.id.eq(memberId),
+                itemType != null ? item.type.eq(itemType) : null
+            )
+            .fetch();
+    }
+
+    @Override
+    public List<Tuple> findEquippedMemberItems(Long memberId) {
+        return queryFactory
+            .select(memberItem, item)
+            .from(memberItem)
+            .join(item).on(memberItem.item.id.eq(item.id))
+            .where(
+                memberItem.member.id.eq(memberId),
+                memberItem.isEquipped.isTrue()
+            )
+            .fetch();
+    }
+}
