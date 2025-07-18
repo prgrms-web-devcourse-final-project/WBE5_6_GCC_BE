d7228dfb8f4714ffe95bb323063ad7a9c4bef451
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-18 14:54:38 +0900

    ::sparkles: feat :Add query to fetch RoutineSchedule with Routine and Member by ID

--- src/main/java/com/honlife/core/app/model/routine/repos/RoutineScheduleRepository.java
+++ src/main/java/com/honlife/core/app/model/routine/repos/RoutineScheduleRepository.java
@@ -2,9 +2,11 @@ package com.honlife.core.app.model.routine.repos;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.honlife.core.app.model.routine.domain.Routine;
 import com.honlife.core.app.model.routine.domain.RoutineSchedule;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface RoutineScheduleRepository extends JpaRepository<RoutineSchedule, Long> {
@@ -19,4 +21,8 @@ public interface RoutineScheduleRepository extends JpaRepository<RoutineSchedule
   List<RoutineSchedule> findByRoutine(Routine routine);
 
   boolean existsByRoutineAndDate(Routine routine, LocalDate today);
+
+  @Query("SELECT rs FROM RoutineSchedule rs JOIN FETCH rs.routine r JOIN FETCH r.member WHERE rs.id = :id")
+  RoutineSchedule findWithRoutineAndMemberById(Long id);
+
 }
