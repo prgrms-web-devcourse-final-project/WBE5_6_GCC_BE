b226f64536316b3db0aecf3fcc504b408292dfb1
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-18 14:50:19 +0900

    feat : Add method to find point by policy type in repository

--- src/main/java/com/honlife/core/app/model/point/repos/PointPolicyRepository.java
+++ src/main/java/com/honlife/core/app/model/point/repos/PointPolicyRepository.java
@@ -2,7 +2,11 @@ package com.honlife.core.app.model.point.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.honlife.core.app.model.point.domain.PointPolicy;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface PointPolicyRepository extends JpaRepository<PointPolicy, Long> {
+
+  PointPolicy findByType(String routine);
 }
