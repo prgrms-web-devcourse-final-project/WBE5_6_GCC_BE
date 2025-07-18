528badab77a85fc76632d5d5daef187c3887ce85
Jun <wnsur1234@gmail.com>
2025-07-18 11:50:10 +0900

    :recycle: refactor: Add JpaRepository to MemberItemRepository & remove unused imports

--- src/main/java/com/honlife/core/app/model/member/repos/MemberItemRepository.java
+++ src/main/java/com/honlife/core/app/model/member/repos/MemberItemRepository.java
@@ -1,19 +1,13 @@
 package com.honlife.core.app.model.member.repos;
 
 
-import com.honlife.core.app.controller.member.payload.MemberItemResponse;
-import com.honlife.core.app.model.item.code.ItemType;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.honlife.core.app.model.item.domain.Item;
 import com.honlife.core.app.model.member.domain.Member;
 import com.honlife.core.app.model.member.domain.MemberItem;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface MemberItemRepository extends JpaRepository<MemberItem, Long> {
+public interface MemberItemRepository extends JpaRepository<MemberItem, Long>, MemberItemRepositoryCustom {
 
     MemberItem findFirstByMember(Member member);
 
