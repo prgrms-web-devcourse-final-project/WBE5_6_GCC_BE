2d18083b706df131ac69cbc0fd9498b96c1a90fb
Jun <wnsur1234@gmail.com>
2025-07-18 11:18:17 +0900

    :recycle: refactor: Remove unused service logic & change return type Response → DTO

--- src/main/java/com/honlife/core/app/model/member/service/MemberItemService.java
+++ src/main/java/com/honlife/core/app/model/member/service/MemberItemService.java
@@ -1,24 +1,24 @@
 package com.honlife.core.app.model.member.service;
 
-import java.util.List;
-
-import com.honlife.core.app.controller.member.payload.MemberItemResponse;
 import com.honlife.core.app.model.item.code.ItemType;
+import com.honlife.core.app.model.item.domain.Item;
 import com.honlife.core.app.model.item.domain.QItem;
+import com.honlife.core.app.model.item.repos.ItemRepository;
+import com.honlife.core.app.model.member.domain.Member;
+import com.honlife.core.app.model.member.domain.MemberItem;
 import com.honlife.core.app.model.member.domain.QMemberItem;
+import com.honlife.core.app.model.member.model.MemberItemDTO;
+import com.honlife.core.app.model.member.model.MemberItemDTOCustom;
+import com.honlife.core.app.model.member.repos.MemberItemRepository;
 import com.honlife.core.app.model.member.repos.MemberItemRepositoryCustom;
+import com.honlife.core.app.model.member.repos.MemberRepository;
+import com.honlife.core.infra.util.NotFoundException;
 import com.querydsl.core.Tuple;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import com.honlife.core.app.model.item.domain.Item;
-import com.honlife.core.app.model.item.repos.ItemRepository;
-import com.honlife.core.app.model.member.domain.Member;
-import com.honlife.core.app.model.member.domain.MemberItem;
-import com.honlife.core.app.model.member.model.MemberItemDTO;
-import com.honlife.core.app.model.member.repos.MemberItemRepository;
-import com.honlife.core.app.model.member.repos.MemberRepository;
-import com.honlife.core.infra.util.NotFoundException;
+
+import java.util.List;
 
 
 @Service
@@ -38,13 +38,13 @@ public class MemberItemService {
      * @param itemType 필터링할 아이템 타입 (null 가능)
      * @return MemberItemResponse 리스트
      */
-    public List<MemberItemResponse> getItemsByMember(Long memberId, ItemType itemType) {
+    public List<MemberItemDTOCustom> getItemsByMember(Long memberId, ItemType itemType) {
         List<Tuple> tuples = memberItemRepositoryCustom.findMemberItems(memberId, itemType);
 
         return tuples.stream().map(tuple -> {
             MemberItem mi = tuple.get(QMemberItem.memberItem);
             Item item = tuple.get(QItem.item);
-            return MemberItemResponse.builder()
+            return MemberItemDTOCustom.builder()
                     .itemKey(item.getItemKey())
                     .itemName(item.getName())
                     .itemDescription(item.getDescription())
@@ -53,17 +53,6 @@ public class MemberItemService {
                     .build();
         }).toList();
     }
-
-    /**
-     * 특정 회원이 현재 장착 중인 아이템 목록을 조회합니다.
-     *
-     * @param memberId 조회할 회원의 ID
-     * @return 장착된 MemberItemResponse 리스트
-     */
-    public List<MemberItemResponse> getEquippedItemsByMember(Long memberId){
-        return memberItemRepository.findEquippedItemsByMemberId(memberId);
-    }
-
 
     public List<MemberItemDTO> findAll() {
         final List<MemberItem> memberItems = memberItemRepository.findAll(Sort.by("id"));
