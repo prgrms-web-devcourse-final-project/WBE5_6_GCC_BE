0ae0ac8c754f65afb8287f4cd0112befd6c890f7
Jun <wnsur1234@gmail.com>
2025-07-18 11:15:13 +0900

    :sparkles: feat: Create custom DTO for response mapping

--- src/main/java/com/honlife/core/app/model/member/model/MemberItemDTOCustom.java
+++ src/main/java/com/honlife/core/app/model/member/model/MemberItemDTOCustom.java
@@ -1,4 +1,19 @@
 package com.honlife.core.app.model.member.model;
 
-public class MemberItemDtoCustom {
+import com.honlife.core.app.model.item.code.ItemType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class MemberItemDTOCustom {
+    private String itemKey;
+    private String itemName;
+    private ItemType itemtype;
+    private String itemDescription;
+    private Boolean isEquipped;
 }
