77d3a993c5b3efafbb563a79805940870086b00b
IDIOcoder <newon.jsw@gmail.com>
2025-07-20 02:23:37 +0900

    :recycle: refactor: redirect to swagger doc

--- src/main/java/com/honlife/core/app/controller/HomeResource.java
+++ src/main/java/com/honlife/core/app/controller/HomeResource.java
@@ -1,15 +1,15 @@
 package com.honlife.core.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class HomeResource {
 
     @GetMapping("/")
     public String index() {
-        return "\"Hello World!\"";
+        return "redirect:/swagger-ui/index.html";
     }
 
 }
