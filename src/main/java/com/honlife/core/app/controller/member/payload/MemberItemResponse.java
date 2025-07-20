d9e9c88ff1f17e11c1fa01e421edb24e623324ab
Jun <wnsur1234@gmail.com>
2025-07-20 14:49:26 +0900

    :sparkles: Feat: Add item existence check logic in MemberItemController

--- src/main/java/com/honlife/core/app/controller/member/payload/MemberItemResponse.java
+++ src/main/java/com/honlife/core/app/controller/member/payload/MemberItemResponse.java
@@ -16,6 +16,7 @@ public class MemberItemResponse {
     private ItemType itemtype;
     private String itemDescription;
     private Boolean isEquipped;
+    private Boolean isActive;
 
     public static MemberItemResponse fromDTO(MemberItemDTOCustom dto) {
         return MemberItemResponse.builder()
@@ -24,6 +25,7 @@ public class MemberItemResponse {
                 .itemtype(dto.getItemtype())
                 .itemDescription(dto.getItemDescription())
                 .isEquipped(dto.getIsEquipped())
+                .isActive(dto.getIsActive())
                 .build();
     }
     public static List<MemberItemResponse> fromDTOList(List<MemberItemDTOCustom> dtos) {
