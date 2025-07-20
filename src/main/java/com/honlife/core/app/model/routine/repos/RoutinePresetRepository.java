a5c5ebc9fce5e1439cf3f34d551ca9e87187fea6
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-20 15:29:43 +0900

    :sparkles: feat: Add query to fetch routine presets with category via fetch join

--- src/main/java/com/honlife/core/app/model/routine/repos/RoutinePresetRepository.java
+++ src/main/java/com/honlife/core/app/model/routine/repos/RoutinePresetRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.honlife.core.app.model.category.domain.Category;
 import com.honlife.core.app.model.routine.domain.RoutinePreset;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface RoutinePresetRepository extends JpaRepository<RoutinePreset, Long> {
@@ -16,4 +18,12 @@ public interface RoutinePresetRepository extends JpaRepository<RoutinePreset, Lo
      * @return {@link RoutinePreset} 을 리스트로 반환합니다.
      */
     List<RoutinePreset> getRoutinePresetByCategoryId(Long categoryId);
+
+    /** 지연 로딩으로 인해 fetchjoin을 사용하였습니다 **/
+    @Query("SELECT rp FROM RoutinePreset rp JOIN FETCH rp.category WHERE (:categoryId IS NULL OR rp.category.id = :categoryId)")
+    List<RoutinePreset> getRoutinePresetWithCategoryId(@Param("categoryId") Long categoryId);
+
+    @Query("SELECT rp FROM RoutinePreset rp JOIN FETCH rp.category")
+    List<RoutinePreset> findAllWithCategory();
+
 }
