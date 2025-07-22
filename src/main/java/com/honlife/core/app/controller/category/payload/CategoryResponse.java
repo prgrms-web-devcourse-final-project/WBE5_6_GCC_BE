71b45bd8c7574192216774b3ca3672c9cb2fe7ef
Kminseo <childstone@users.noreply.github.com>
2025-07-22 09:35:36 +0900

    ✨ feat: update response DTO to reflect new Column

--- src/main/java/com/honlife/core/app/controller/category/payload/CategoryResponse.java
+++ src/main/java/com/honlife/core/app/controller/category/payload/CategoryResponse.java
@@ -1,7 +1,9 @@
 package com.honlife.core.app.controller.category.payload;
 
 import com.honlife.core.app.model.category.code.CategoryType;
+import com.honlife.core.app.model.category.dto.CategoryDTO;
 import com.honlife.core.app.model.category.dto.CategoryUserViewDTO;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,28 +15,22 @@ public class CategoryResponse {
 
     private Long categoryId;
 
-    private Long parentId;
-
-    private String parentName;
-
     private String categoryName;
 
     private CategoryType categoryType;
 
     private String emoji;
 
-    private Long memberId;
+    private List<ChildCategoryPayload> children;
 
 
-    public static CategoryResponse fromDTO(CategoryUserViewDTO categoryDTO) {
+    public static CategoryResponse fromDTO(CategoryDTO categoryDTO) {
         return CategoryResponse.builder()
             .categoryId(categoryDTO.getId())
             .categoryName(categoryDTO.getName())
             .categoryType(categoryDTO.getType())
             .emoji(categoryDTO.getEmoji())
-            .parentId(categoryDTO.getParentId())
-            .parentName(categoryDTO.getParentName())
-            .memberId(categoryDTO.getMember())
+            .children(ChildCategoryPayload.fromDTO(categoryDTO.getChildren()))
             .build();
     }
 
