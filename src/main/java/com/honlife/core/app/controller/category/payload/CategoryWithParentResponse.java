22c81a9e6ea5fec86f8d701853d15c33e2724995
Kminseo <childstone@users.noreply.github.com>
2025-07-22 12:03:58 +0900

    :sparkles: add CategoryWithParentResponse
    id를 통한 검색 시 SUB 카테고리를 검색하였을 때 parent에 대한 정보도 함께 반환하기 위해 추가하였습니다.

---
+++ src/main/java/com/honlife/core/app/controller/category/payload/CategoryWithParentResponse.java
@@ -0,0 +1,39 @@
+package com.honlife.core.app.controller.category.payload;
+
+import com.honlife.core.app.model.category.code.CategoryType;
+import com.honlife.core.app.model.category.dto.CategoryDTO;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CategoryWithParentResponse {
+
+    private Long categoryId;
+
+    private String categoryName;
+
+    private CategoryType categoryType;
+
+    private String emoji;
+
+    private Long parent;
+
+    private List<ChildCategoryPayload> children;
+
+
+
+    public static CategoryWithParentResponse fromDTO(CategoryDTO categoryDTO) {
+        return CategoryWithParentResponse.builder()
+            .categoryId(categoryDTO.getId())
+            .categoryName(categoryDTO.getName())
+            .categoryType(categoryDTO.getType())
+            .emoji(categoryDTO.getEmoji())
+            .parent(categoryDTO.getParent())
+            .children(ChildCategoryPayload.fromDTO(categoryDTO.getChildren()))
+            .build();
+    }
+
+
+}
