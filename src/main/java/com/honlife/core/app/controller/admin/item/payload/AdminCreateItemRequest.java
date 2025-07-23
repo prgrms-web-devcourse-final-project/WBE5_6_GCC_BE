3cb5573666740c09a429f54d312c312ee0fde020
Jun <wnsur1234@gmail.com>
2025-07-23 21:06:00 +0900

    :sparkles: Feat: Add Request for CreateItem

--- src/main/java/com/honlife/core/app/controller/admin/item/payload/AdminCreateItemRequest.java
+++ src/main/java/com/honlife/core/app/controller/admin/item/payload/AdminCreateItemRequest.java
@@ -1,4 +1,13 @@
 package com.honlife.core.app.controller.admin.item.payload;
 
+import com.honlife.core.app.model.item.code.ItemType;
+import lombok.Data;
+
+@Data
 public class AdminCreateItemRequest {
+    private String itemKey;
+    private ItemType itemType;
+    private String itemName;
+    private String itemDescription;
+    private Integer itemPrice;
 }
