f919aa05ba38ec36567652708688bf1d48309fb9
SeongWon Jeong <newon.jsw@gmail.com>
2025-07-24 05:09:44 +0900

    :recycle: refactor: weekly and event quest entities
    변경된 ERD 구조에 맞추어 퀘스트 테이블들을 재정의 하였습니다.
    주간 퀘스트에서는 퀘스트 상세정보인 info 필드가 사라지고, 퀘스트 종류인 type, 카테고리 종류인 category_id, 목표치인 target 필드들이 추가되었습니다. 또한, 카테고리를 제외한 필드들을 nullable=false 로 처리하였습니다.

--- src/main/java/com/honlife/core/app/model/quest/domain/EventQuest.java
+++ src/main/java/com/honlife/core/app/model/quest/domain/EventQuest.java
@@ -1,10 +1,17 @@
 package com.honlife.core.app.model.quest.domain;
 
+import com.honlife.core.app.model.category.domain.Category;
+import com.honlife.core.app.model.quest.code.QuestType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -32,19 +39,27 @@ public class EventQuest extends BaseEntity {
     )
     private Long id;
 
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String key;
 
-    @Column
+    @Column(nullable = false)
     private String name;
 
-    @Column(columnDefinition = "text")
-    private String info;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private QuestType type;
 
-    @Column
+    @Column(nullable = false)
+    private Integer target;
+
+    @Column(nullable = false)
     private LocalDateTime startDate;
 
-    @Column
+    @Column(nullable = false)
     private LocalDateTime endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 
 }
