cf722af363bcda1fb89fae6bcc468db6f8ae1278
Jun <wnsur1234@gmail.com>
2025-07-23 22:18:54 +0900

    :memo: Docs: Update Swagger API documentation

---
+++ src/main/java/com/honlife/core/app/controller/admin/item/payload/AdminCreateItemRequeset.java
@@ -0,0 +1,38 @@
+package com.honlife.core.app.controller.admin.item.payload;
+import com.honlife.core.app.model.item.code.ItemType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Schema(name = "ItemCreateRequestDTO", description = "상점 아이템 추가 요청 DTO")
+public class AdminCreateItemRequeset {
+
+    @NotBlank
+    @Size(max = 50)
+    @Schema(description = "아이템 키 (고유값)", example = "top_item_01")
+    private String itemKey;
+
+    @NotBlank
+    @Size(max = 50)
+    @Schema(description = "아이템 이름", example = "청소 모자")
+    private String itemName;
+
+    @NotNull
+    @Schema(description = "아이템 가격", example = "300")
+    private Integer price;
+
+    @NotNull
+    @Schema(description = "아이템 타입", example = "TOP")
+    private ItemType itemType;
+
+    @NotNull
+    @Schema(description = "아이템 설명", example = "청소 성능이 +1 증가합니다.")
+    private String itemDescription;
+}
