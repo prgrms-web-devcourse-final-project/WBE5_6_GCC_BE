3979c5a759c1a5d589fee2e55b0657c97cd3ae7a
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-23 14:22:49 +0900

    :art: style: Clean up and reorder fields in today response and detail DTOs

---
+++ src/main/java/com/honlife/core/app/model/routine/dto/RoutineTodayItemDTO.java
@@ -0,0 +1,33 @@
+package com.honlife.core.app.model.routine.dto;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class RoutineTodayItemDTO {
+  private Long scheduleId;
+
+
+  private Long routineId;
+
+
+  private String majorCategory;
+
+
+  private String subCategory;
+
+
+  private String name;
+
+
+  private String triggerTime;
+
+
+  private Boolean isDone;
+
+
+  private Boolean isImportant;
+
+}
