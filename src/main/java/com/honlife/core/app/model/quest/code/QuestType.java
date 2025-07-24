123a4ba2226a70794a5c1ddbe7f51429df3da188
SeongWon Jeong <newon.jsw@gmail.com>
2025-07-24 10:10:27 +0900

    :fire: remove: unused enum type

--- src/main/java/com/honlife/core/app/model/quest/code/QuestType.java
+++ src/main/java/com/honlife/core/app/model/quest/code/QuestType.java
@@ -7,11 +7,6 @@ public enum QuestType {
      */
     STREAK,
     /**
-     * 하루 기준 연속<br>
-     * ex) 3일 연속 ㅇㅇ카테고리 루틴 완료
-     */
-    DAY_STREAK,
-    /**
      * 누적<br>
      * ex) 이번주 ㅇㅇ카테고리 누적 n회
      */
