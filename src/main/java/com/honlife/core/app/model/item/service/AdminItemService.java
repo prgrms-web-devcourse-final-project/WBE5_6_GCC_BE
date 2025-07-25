dcc9dda02cb0fba0e46419b5218de26ab63649a9
Jun <wnsur1234@gmail.com>
2025-07-25 18:00:53 +0900

    :twisted_rightwards_arrows: merge: Fix logic during merge conflict resolution

--- src/main/java/com/honlife/core/app/model/item/service/AdminItemService.java
+++ src/main/java/com/honlife/core/app/model/item/service/AdminItemService.java
@@ -3,17 +3,23 @@ package com.honlife.core.app.model.item.service;
 import com.honlife.core.app.controller.admin.item.payload.AdminCreateItemRequest;
 import com.honlife.core.app.model.item.domain.Item;
 import com.honlife.core.app.model.item.repos.ItemRepository;
+import com.honlife.core.app.model.member.domain.MemberItem;
+import com.honlife.core.app.model.member.service.MemberItemService;
 import com.honlife.core.infra.error.exceptions.CommonException;
 import com.honlife.core.infra.response.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AdminItemService {
 
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+    private final ItemService itemService;
+    private final MemberItemService memberItemService;
 
     /**
      * 관리자 전용 아이템 생성 로직
@@ -38,5 +44,24 @@ public class AdminItemService {
                 .isListed(request.getIsListed())
                 .build();
         itemRepository.save(item);
+    }
+
+    /**
+     * 아이템을 Soft Delete 처리합니다.
+     * itemKey에 해당하는 아이템을 조회하여 isActive 값을 false로 설정합니다.
+     *
+     * @param itemKey 삭제할 아이템의 고유 키
+     */
+    @Transactional
+    public void softDeleteItem(String itemKey) {
+
+        Item item = itemService.getItemByKey(itemKey);
+        if(item == null) {
+            throw new CommonException(ResponseCode.NOT_FOUND_ITEM);
+        }
+        item.setIsActive(false);
+
+        List<MemberItem> memberItems = memberItemService.findAllByItem(item);
+        memberItems.forEach((memberItem) -> {memberItem.setIsActive(false);});
     }
 }
