204e67e330b435bfa0d1fcae61406a14a649d851
Kminseo <childstone@users.noreply.github.com>
2025-07-26 03:35:27 +0900

    ✨ feat: create DashboardService skeleton for weekly dashboard

---
+++ src/main/java/com/honlife/core/app/model/dashboard/service/DashboardService.java
@@ -0,0 +1,10 @@
+package com.honlife.core.app.model.dashboard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DashboardService {
+
+}
