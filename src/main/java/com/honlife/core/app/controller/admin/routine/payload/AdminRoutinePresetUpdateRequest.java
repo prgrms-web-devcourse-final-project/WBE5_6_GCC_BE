344681b80afcdd4dc177462cfec09e25dff00830
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-26 15:25:05 +0900

    ::art: feat :Created separate DTO for update requests

---
+++ src/main/java/com/honlife/core/app/controller/admin/routine/payload/AdminRoutinePresetUpdateRequest.java
@@ -0,0 +1,37 @@
+package com.honlife.core.app.controller.admin.routine.payload;
+
+import com.honlife.core.app.model.routine.code.RepeatType;
+
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AdminRoutinePresetUpdateRequest {
+
+  private Long categoryId;
+
+  private String majorCategory;
+
+  private String subCategory;
+
+  private String triggerTime;
+
+  private Boolean isImportant;
+
+  private RepeatType repeatType;
+
+  private String repeatValue;
+
+  private LocalDate startRoutineDate;
+
+  @Size(max = 50, message = "루틴 내용은 50자를 초과할 수 없습니다")
+  private String name;
+
+  private Integer repeatInterval ;
+
+}
