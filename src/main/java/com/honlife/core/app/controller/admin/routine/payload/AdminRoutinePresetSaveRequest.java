ccecdc3a20b12ce8dab21ea14dee7ba36017e05c
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-26 15:24:01 +0900

    ::art: style :Added request fields for receiving additional values

--- src/main/java/com/honlife/core/app/controller/admin/routine/payload/AdminRoutinePresetSaveRequest.java
+++ src/main/java/com/honlife/core/app/controller/admin/routine/payload/AdminRoutinePresetSaveRequest.java
@@ -1,26 +1,40 @@
 package com.honlife.core.app.controller.admin.routine.payload;
 
+import com.honlife.core.app.model.routine.code.RepeatType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Schema(description = "추천 루틴 프리셋 생성/수정 요청")
 public class AdminRoutinePresetSaveRequest {
 
   @NotNull(message = "카테고리는 필수입니다")
-  @Schema(description = "카테고리 ID", example = "1", required = true)
   private Long categoryId;
+
+  private String majorCategory;
+
+  private String subCategory;
+
+  private String triggerTime;
+
+  private Boolean isImportant;
+
+  @NotNull(message = "반복 유형은 필수입니다")
+  private RepeatType repeatType;
+
+  private String repeatValue;
+
+  @NotNull(message = "루틴 시작 날짜는 필수입니다")
+  private LocalDate startRoutineDate;
 
   @NotBlank(message = "루틴 내용은 필수입니다")
   @Size(max = 50, message = "루틴 내용은 50자를 초과할 수 없습니다")
-  @Schema(description = "루틴 내용", example = "아침 스트레칭 하기", required = true)
-  private String content;
+  private String name;
 
-  @Schema(description = "활성화 여부", example = "true")
-  private Boolean isActive = true;
+  private int repeatInterval = 1;
 }
