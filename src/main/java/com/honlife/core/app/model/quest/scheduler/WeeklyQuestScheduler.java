38991b3a1368bee384130346681c5721becdf7f4
SeongWon Jeong <newon.jsw@gmail.com>
2025-07-27 15:39:17 +0900

    :recycle: refactor: enable async to deactivate weekly quests scheduler

--- src/main/java/com/honlife/core/app/model/quest/scheduler/WeeklyQuestScheduler.java
+++ src/main/java/com/honlife/core/app/model/quest/scheduler/WeeklyQuestScheduler.java
@@ -2,6 +2,7 @@ package com.honlife.core.app.model.quest.scheduler;
 
 import com.honlife.core.app.model.quest.service.WeeklyQuestProgressService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ public class WeeklyQuestScheduler {
      * 매주 월요일 0시에 실행 (cron: 초 분 시 일 월 요일)
      * 매주 월요일 0시 0분 0초
      */
+    @Async
     @Scheduled(cron = "0 0 0 * * MON", zone = "Asia/Seoul")
     public void deactivatePreviousWeeklyQuests() {
         weeklyQuestProgressService.deactivatePreviousWeeklyQuests();
