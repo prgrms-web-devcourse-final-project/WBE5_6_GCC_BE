32ab0527c434efceb512e596b0de4ca9bb775e8f
Kminseo <childstone@users.noreply.github.com>
2025-07-28 00:42:16 +0900

    :recycle: refactor: Enhance weekly AI comment generation

--- src/main/java/com/honlife/core/app/model/dashboard/scheduler/MemberDashboardScheduler.java
+++ src/main/java/com/honlife/core/app/model/dashboard/scheduler/MemberDashboardScheduler.java
@@ -1,12 +1,16 @@
 package com.honlife.core.app.model.dashboard.scheduler;
 
+import com.honlife.core.app.model.dashboard.service.AICommentService;
 import com.honlife.core.app.model.dashboard.service.DashboardService;
 import com.honlife.core.app.model.member.domain.Member;
 import com.honlife.core.app.model.member.repos.MemberRepository;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,22 +24,23 @@ public class MemberDashboardScheduler {
 
 
     private final MemberRepository memberRepository;
-    private final DashboardService dashboardService;
+    private final AICommentService aiCommentService;
 
     /**
      * 일 -> 월 넘어갈 때 AI comment를 생성
      */
+    @Async
     @Transactional
     @Scheduled(cron = "0 0 0 ? * MON")
     public void createAIComment() {
         log.info("주간 리포트를 위한 AI 조언을 생성 중입니다...");
         List<Member> members = memberRepository.findAllByIsActive(true);
 
-        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
-
+        LocalDate endDate = LocalDate.now().minusDays(1);
+        LocalDate startDate = endDate.with(DayOfWeek.MONDAY);
         // 활성화된 member들의 ai comment를 만듭니다.
         for (Member member : members) {
-            dashboardService.getDashBoardData(member.getEmail(), yesterday);
+            aiCommentService.getOrCreateAIComment(member.getEmail(),startDate, endDate);
         }
 
     }
