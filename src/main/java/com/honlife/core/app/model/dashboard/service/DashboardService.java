598187426a278fadbee0b70e3fda9cdf3d0cc043
Kminseo <childstone@users.noreply.github.com>
2025-07-28 17:37:28 +0900

    🔥 remove: unused annotation

--- src/main/java/com/honlife/core/app/model/dashboard/service/DashboardService.java
+++ src/main/java/com/honlife/core/app/model/dashboard/service/DashboardService.java
@@ -15,12 +15,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-@Slf4j
 public class DashboardService {
 
     private final RoutineScheduleRepository routineScheduleRepository;
