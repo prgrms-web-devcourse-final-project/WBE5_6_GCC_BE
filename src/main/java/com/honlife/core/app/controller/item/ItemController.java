e9be8f4db49f2bd0121686c7ba42e160e998513e
Jun <wnsur1234@gmail.com>
2025-07-29 10:42:05 +0900

    :art: style: Replace wildcard imports with explicit class imports

--- src/main/java/com/honlife/core/app/controller/item/ItemController.java
+++ src/main/java/com/honlife/core/app/controller/item/ItemController.java
@@ -15,7 +15,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
