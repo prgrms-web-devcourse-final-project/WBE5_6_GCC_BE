69f3ab7c413e1d512e0df8bbbc546bbb2ae21a3b
NEWON <108709378+IDIOcoder@users.noreply.github.com>
2025-07-30 00:04:25 +0900

    Update HomeResource.java

--- src/main/java/com/honlife/core/app/controller/HomeResource.java
+++ src/main/java/com/honlife/core/app/controller/HomeResource.java
@@ -9,8 +9,7 @@ public class HomeResource {
 
     @GetMapping("/")
     public String index() {
-//        return "\"Hello World!\"";
-        return "<a href=\"/oauth2/authorization/google\">구글 로그인</a>";
+        return "\"Hello World!\"";
     }
 
 }
