e284b04dfd129ea0e4bf75415cc4bad058837530
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-29 22:50:10 +0900

    :heavy_plus_sign: chore: Add All NotNull

--- src/main/java/com/honlife/core/app/controller/admin/routine/payload/AdminRoutinePresetUpdateRequest.java
+++ src/main/java/com/honlife/core/app/controller/admin/routine/payload/AdminRoutinePresetUpdateRequest.java
@@ -2,6 +2,7 @@ package com.honlife.core.app.controller.admin.routine.payload;
 
 import com.honlife.core.app.model.routine.code.RepeatType;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Builder;
@@ -13,21 +14,28 @@ import lombok.Setter;
 @Builder
 public class AdminRoutinePresetUpdateRequest {
 
+  @NotNull
   private Long categoryId;
 
+  @NotNull
   private String triggerTime;
 
+  @NotNull
   private Boolean isImportant;
 
+  @NotNull
   private RepeatType repeatType;
 
+  @NotNull
   private String repeatValue;
 
+  @NotNull
   private LocalDate initDate;
 
   @Size(max = 50, message = "루틴 내용은 50자를 초과할 수 없습니다")
   private String name;
 
+  @NotNull
   private Integer repeatTerm ;
 
 }
