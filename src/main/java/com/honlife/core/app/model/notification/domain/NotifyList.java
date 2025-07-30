568d6e00c6851b94c9cc69e352fc18f3830f3d6c
NEWON <108709378+IDIOcoder@users.noreply.github.com>
2025-07-30 11:18:10 +0900

    Update NotifyList.java

--- src/main/java/com/honlife/core/app/model/notification/domain/NotifyList.java
+++ src/main/java/com/honlife/core/app/model/notification/domain/NotifyList.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 public class NotifyList extends BaseEntity {
 
   @Id
+  @Column(nullable = false, updatable = false)
   @SequenceGenerator(
       name = "notify_sequence",
       sequenceName = "notify_sequence",
@@ -25,7 +26,6 @@ public class NotifyList extends BaseEntity {
       strategy = GenerationType.SEQUENCE,
       generator = "notify_sequence"
   )
-  @Column(nullable = false, updatable = false)
   private Long id;
 
   @Column(nullable = false)
