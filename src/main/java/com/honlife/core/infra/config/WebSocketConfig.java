288cffc4c43c637454c83c14a92e7a8710f8abb2
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-07-30 11:12:00 +0900

    :recycle: refactor: url fix and delete initDate

--- src/main/java/com/honlife/core/infra/config/WebSocketConfig.java
+++ src/main/java/com/honlife/core/infra/config/WebSocketConfig.java
@@ -5,16 +5,21 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+  @Value("${app.domain}")
+  private String appDomain;
+
+
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws/connect")//웹소켓 연결할때 사용할 api
-        .setAllowedOrigins("http://localhost:8083")
+        .setAllowedOrigins(appDomain)
         .withSockJS();
   }
 
