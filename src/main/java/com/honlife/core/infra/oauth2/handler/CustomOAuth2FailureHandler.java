0914567945565f446c42c9c5b524871cacd6c2dd
SeongWon Jeong <newon.jsw@gmail.com>
2025-08-03 16:43:16 +0900

    :recycle: refactor: correct font domain value

--- src/main/java/com/honlife/core/infra/oauth2/handler/CustomOAuth2FailureHandler.java
+++ src/main/java/com/honlife/core/infra/oauth2/handler/CustomOAuth2FailureHandler.java
@@ -15,7 +15,7 @@ public class CustomOAuth2FailureHandler extends SimpleUrlAuthenticationFailureHa
 
     private static final Logger log = LoggerFactory.getLogger(CustomOAuth2FailureHandler.class);
 
-    @Value("${front-prod.domain}")
+    @Value("${front-server.prod-domain}")
     private String frontDomain;
 
     @Override
